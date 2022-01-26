28714a052ca56d91e27d51843f3076ff63a63983
PJLAB\huanghaian <1286304229@qq.com>
2022-01-26 14:17:38 +0800

    add fcos

--- configs/COCO-Detection/fcos_R_50_FPN_1x.py
+++ configs/COCO-Detection/fcos_R_50_FPN_1x.py
@@ -9,3 +9,16 @@ optimizer.lr = 0.01
 
 model.backbone.bottom_up.freeze_at = 2
 train.init_checkpoint = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
+
+# ceph
+file_client_args = dict(
+    backend='petrel',
+    path_mapping=dict({
+        'datasets/': 's3://openmmlab/datasets/detection/',
+        '.datasets/': 's3://openmmlab/datasets/detection/'
+    }))
+
+# If you don’t need ceph, you can directly comment the following code or
+# set file_client_args to None
+dataloader.train.mapper.file_client_args = file_client_args
+dataloader.test.mapper.file_client_args = file_client_args
